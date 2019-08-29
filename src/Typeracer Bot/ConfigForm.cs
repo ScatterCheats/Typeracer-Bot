14c5777c0672c5e1db8aded803369439561b5eb0
VollRahm <vollrahm@scatter-cheats.com>
2019-08-28 22:42:03 -0300

    - Fixed Race Start Detection

--- src/Typeracer Bot/ConfigForm.cs
+++ src/Typeracer Bot/ConfigForm.cs
@@ -37,6 +37,7 @@ namespace Typeracer_Bot
             options.AddArgument("--start-maximized");
             options.AddArgument("--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.100 Safari/537.36");
             options.AddExcludedArguments(new List<string>() { "enable-automation" });
+            options.PageLoadStrategy = PageLoadStrategy.None;
 
             chrome = new ChromeDriver(driverBuilder, options);
             botThread_obj = new BotThread(chrome);
