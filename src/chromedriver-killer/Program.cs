1274819b1e9961c3e91fbb014eac7ebbd2e39165
VollRahm <vollrahm@scatter-cheats.com>
2019-08-29 00:41:35 -0300

    - Added chromedriver-killer to kill all chromedriver.exes while debugging. Only for my use.

---
+++ src/chromedriver-killer/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using System.Linq;
+
+namespace chromedriver_killer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Process.GetProcessesByName("chromedriver").ToList().ForEach(proc => proc.Kill());
+        }
+    }
+}
